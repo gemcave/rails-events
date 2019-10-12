2d3a89a2fdd7a1f2b90f3a21c94080f619564978
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-10-12 17:09:08 +0600

    view was updated with helpers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_12_080248) do
+ActiveRecord::Schema.define(version: 2019_10_12_084419) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_10_12_080248) do
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "starts_at"
+    t.text "description"
   end
 
 end
