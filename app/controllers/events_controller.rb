1e4d44fd37319d1a77ca09dd9762b148f76ec7bd
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-10-12 20:30:28 +0600

    create event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,10 +12,25 @@ class EventsController < ApplicationController
 	def edit
 		@event = Event.find(params[:id])
 	end
+
 	def update
 		@event = Event.find(params[:id])
-		event_params = params.require(:event).permit(:name,:description,:starts_at,:price,:location)
 		@event.update(event_params)
 		redirect_to @event
+	end
+
+	def new
+		@event = Event.new
+	end
+
+	def create
+		@event = Event.new(event_params)
+		@event.save
+		redirect_to @event
+	end
+
+	private
+	def event_params
+		event_params = params.require(:event).permit(:name,:description,:starts_at,:price,:location)
 	end
 end
