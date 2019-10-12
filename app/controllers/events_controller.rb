20737771fb7fd45dee21ad8b711eff7023b934ea
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-10-12 20:46:34 +0600

    destroy event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -29,6 +29,12 @@ class EventsController < ApplicationController
 		redirect_to @event
 	end
 
+	def destroy
+		@event = Event.find(params[:id])
+		@event.destroy
+		redirect_to events_url
+	end
+	
 	private
 	def event_params
 		event_params = params.require(:event).permit(:name,:description,:starts_at,:price,:location)
