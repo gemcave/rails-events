11366480b263266582884a06f3a5fa3ec417fa4a
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-10-14 21:53:35 +0600

    authorization

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,16 @@ class SessionsController < ApplicationController
 		if user = User.authenticate(params[:email], params[:password])
 			session[:user_id] = user.id
 			flash[:notice] = "Welcome back, #{user.name}!"
-			redirect_to user
+			redirect_to(session[:intended_url] || user)
+			session[:intended_url] = nil
 		else
 			flash.now[:alert] = "Invalid email/password combination!"
 			render :new
 		end
+	end
+
+	def destroy
+		session[:user_id] = nil
+		redirect_to root_url, notice: "Now you're signed out!"
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+	before_action :require_signin, except: [:new, :create]
+	before_action :require_correct_user, only: [:edit, :destroy, :update]
+
 	def index
 		@users = User.all
 	end
@@ -22,11 +25,9 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
 		if @user.update(user_params)
 			redirect_to @user, notice: "Account successfully updated!"
 		else
@@ -35,8 +36,8 @@ class UsersController < ApplicationController
 	end
 
 	def destroy
-		@user = User.find(params[:id])
 		@user.destroy
+		session[:user_id] = nil
 		redirect_to root_url, alert: "Account successfully deleted!"
 	end
 
@@ -45,4 +46,13 @@ class UsersController < ApplicationController
 	def user_params
 		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
+
+	def require_correct_user
+		@user = User.find(params[:id])
+		unless current_user?(@user)
+			redirect_to root_url
+		end
+	end
 end
+
+
