b68c1e7c2e2d92c75c8e4e81d91fca840a1945b4
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-10-16 21:27:47 +0600

    custom routes for scopes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,22 @@ Rails.application.routes.draw do
   resources :categories
 	resource :session
 	get "signup" => "users#new"
-  resources :users
+	resources :users
+	
 	root "events#index"
   # get "events" => "events#index"
   # get "events/:id" => "events#show", as: "event"
   # get "events/:id/edit" => "events#edit", as: "edit_event"
 	# patch "events/:id" => "events#update"
 	
+	# %w(free past).each do |scope|
+	# 	get "events/#{scope}" => "events#index", scope: scope
+	# end
+
+	# get "events/:scope" => "events#index", constraints: {scope: /past|free/}
+
+	get "events/filter/:scope" => "events#index", as: :filtered_events
+
 	resources :events do
 		resources :registrations 
 		resources :likes
