5392ee516261ae53754461206b817d205ac44579
yerowyn <yerowyn@gmail.com>
2020-04-10 14:58:03 +0600

    config selectize

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,28 @@
 class CategoriesController < ApplicationController
+	skip_before_action :verify_authenticity_token, raise: false
+
+	def index
+		categories = Category.all
+		if current_user_admin?
+			render json: categories.as_json(only: :name), status: :ok
+		else
+			head(:unauthorized)
+		end
+	end
+
+	def create
+		@category = Category.new(category_params)
+		if @category.save
+			render json: {status: 'SUCCESS', message: 'Articles successfuly created', data:@category}, status: :ok	 
+		else
+			render json: {status: 'SUCCESS', message: 'Article NOT successfuly created', data:@category},
+			 status: :unprocessable_entity
+		end
+	end
+
+
+	private
+	def category_params
+		params.permit(:name)
+	end
 end
--- app/javascript/packs/tags.js
+++ app/javascript/packs/tags.js
@@ -1,19 +1,36 @@
 import 'selectize'
 import $ from 'jquery';
 
-$(document).on("turbolinks:load", function() {
-		$(function () {
-			console.log('loaded')
-		})
-
-		$('.selectize').selectize({
-    delimiter: ',',
-    persist: false,
-    create: function(input) {
-        return {
-            value: input,
-            text: input
-        }
-    }
-	});
+$(document).on("turbolinks:load", function() {	
+	$('.selectize').selectize({
+		delimiter: ',',
+		persist: false,
+		placeholder: '-',
+		create: function (input, callback){
+			var self = this;
+			var fd = new FormData();
+			fd.append("name", input)			
+			console.log(input)
+			console.log(self.options)
+			for (let [key, value] of Object.entries(self.options)) {
+				if (value.text == input) {
+					self.placeholder = "-"
+					return callback();
+				}
+			}
+			$.ajax({
+						url: '/categories/',
+						type: 'post',
+						data: fd,
+						processData: false,
+            contentType: false,
+						success: function (result) {
+								var res = { value: result.data.id, text: result.data.name }
+								if (res) {
+									 return callback({ value: result.data.id, text: result.data.name });
+								}
+						}
+				});
+	 },    
+	})
 })
