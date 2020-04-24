fda6585c524aebfd3278f857e7b8511710b1505d
yerowyn <yerowyn@gmail.com>
2020-04-24 15:29:37 +0600

    fix:small user admin view clean

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,12 +1,12 @@
 ActiveAdmin.register User do
-	permit_params :name, :email, :password, :admin, :encrypted_password, :reset_password_token, :reset_password_sent_at
+	permit_params :name, :email, :password, :admin, :reset_password_token, :reset_password_sent_at
 
 	form do |f|
 		f.inputs "Admin Details" do
 		f.input :name
 		f.input :email
 		f.input :password
-		f.input :encrypted_password
+		# f.input :encrypted_password
 		f.input :reset_password_token
 		f.input :reset_password_sent_at
 		# f.input :password_confirmation
