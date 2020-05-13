e6f477c2a2d4e2a4537a5c41d89540e41827060c
yerowyn <yerowyn@gmail.com>
2020-05-13 15:22:04 +0600

    fix sg: delete config

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
 
-ActionMailer::Base.smtp_settings = {
-	:user_name => 'apikey',
-	:password => Rails.application.credentials.SG_API_KEY,
-	:domain => 'blooming-garden-65144.herokuapp.com',
-	:address => 'smtp.sendgrid.net',
-	:port => 465,
-	:authentication => :plain,
-	:enable_starttls_auto => true
-}
-
 # Initialize the Rails application.
 Rails.application.initialize!
