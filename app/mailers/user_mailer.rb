e6b557111ce809a687334742eeb03529e8c6374c
yerowyn <yerowyn@gmail.com>
2020-05-13 18:27:51 +0600

    edit sg: domain and host

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ApplicationMailer
 
 	def password_reset(user)
 			@user = user 
-			mail :to => user.email, :subject => "Password Reset"
-	end 
+			mail :to => user.email, :subject => "Password Reset", content_type => "text/html"
+	end
 end
