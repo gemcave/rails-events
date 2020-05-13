5466aebc21029d083f1e5e34ac62bb35961706d7
yerowyn <yerowyn@gmail.com>
2020-05-13 15:11:17 +0600

    add sg mail config

--- config/environment.rb
+++ config/environment.rb
@@ -1,26 +1,15 @@
 # Load the Rails application.
 require_relative 'application'
 
-# ActionMailer::Base.smtp_settings = {
-# 	:address        => 'smtp.sendgrid.net',
-# 	:port           => '465',
-# 	:authentication => :plain,
-# 	:user_name => ENV['SENDGRID_PASSWORD'],
-# 	:password => ENV['SENDGRID_PASSWORD'],
-# 	:domain         => 'localhost:3000',
-# 	:enable_starttls_auto => true
-# }
-
-  # ActionMailer::Base.smtp_settings = {
-  #   :address        => 'smtp.sendgrid.net',
-  #   :port           => 465,
-  #   :authentication => :plain,
-  #   :user_name => ENV['SENDGRID_PASSWORD'],
-	# 	:password => ENV['SENDGRID_PASSWORD'],
-  #   :domain         => 'localhost:3000',
-  #   :enable_starttls_auto => true
-	# }
-	
+ActionMailer::Base.smtp_settings = {
+	:user_name => 'apikey',
+	:password => Rails.application.credentials.SG_API_KEY,
+	:domain => 'blooming-garden-65144.herokuapp.com',
+	:address => 'smtp.sendgrid.net',
+	:port => 465,
+	:authentication => :plain,
+	:enable_starttls_auto => true
+}
 
 # Initialize the Rails application.
 Rails.application.initialize!
